8e43fd20a2a4f48c9288b5eb12e379390f1cf2da
JuanSensio <juansensio03@gmail.com>
2018-04-16 18:32:32 +0200

    commit

--- webcam.js
+++ webcam.js
@@ -4,15 +4,51 @@ var video = document.getElementById('video');
 
 function start()
 {
-	var constraints = { audio: false, video: true }; 
-	navigator.mediaDevices.getUserMedia(constraints)
-	.then(function(mediaStream) {
-	  video.srcObject = mediaStream;
-	  video.onloadedmetadata = function(e) {
-	    video.play();
-	  };
-	})
-	.catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
+
+    navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+	//var video = document.createElement('video');
+	video.style.width =  document.width + 'px';
+	video.style.height = document.height + 'px';
+	video.setAttribute('autoplay', '');
+	video.setAttribute('muted', '');
+	video.setAttribute('playsinline', '');
+
+	var facingMode = "user";
+
+	var constraints = {
+	  audio: false,
+	  video: {
+	    facingMode: facingMode
+	  }
+	}
+
+	navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+		video.srcObject = stream;
+	});
+
+	//document.body.appendChild(video);
+
+	video.addEventListener('click', function() {
+	  if (facingMode == "user") {
+	    facingMode = "environment";
+	  } else {
+	    facingMode = "user";
+	  }
+	  
+	  constraints = {
+	    audio: false,
+	    video: {
+	      facingMode: facingMode
+	    }
+	  }  
+	  
+	  navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+		  video.srcObject = stream;	
+	  });
+	});
+    
+
 }
 
 start();
