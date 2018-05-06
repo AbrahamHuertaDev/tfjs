f804d24d6732f290643d23a243703716a39d0a5d
MarioProjects <maparla@inf.upv.es>
2018-05-06 19:10:32 +0200

    Sin ajuste para movil?!

--- js/webcam.js
+++ js/webcam.js
@@ -36,7 +36,7 @@ function start() {
 	  
 	  navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
 		  video.srcObject = stream;	
-		  adjustVideoSize(video.width, video.height);
+		  //adjustVideoSize(video.width, video.height);
 	  });
 	});
 
